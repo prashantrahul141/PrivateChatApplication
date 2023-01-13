49036682173cea616f79f673368e2edba22679f8
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 00:36:02 +0530

    adding component

---
+++ src/components/common/loading.tsx
@@ -0,0 +1,13 @@
+import type { FC } from 'react';
+
+const Loading: FC = () => {
+  return (
+    <>
+      <div className='fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-transparent'>
+        <div className='h-6 w-6 animate-[spin_0.75s_ease-in-out_infinite] rounded-full border-4 border-themePrimary-1100/10 border-t-themePrimary-400'></div>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
