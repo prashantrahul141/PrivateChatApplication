32898635bd2a6f8bf6f8327326a02a3e6f0c13d4
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 00:53:33 +0530

    making loading relative to parent

--- src/components/common/loading.tsx
+++ src/components/common/loading.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 const Loading: FC = () => {
   return (
     <>
-      <div className='fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-transparent'>
+      <div className='z-50 flex h-full w-full items-center justify-center bg-transparent'>
         <div className='h-6 w-6 animate-[spin_0.75s_ease-in-out_infinite] rounded-full border-4 border-themePrimary-1100/10 border-t-themePrimary-400'></div>
       </div>
     </>
