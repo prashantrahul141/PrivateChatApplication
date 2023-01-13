1473dde91ceb3352130ebc786cc4bd8c3c71333b
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 02:16:14 +0530

    adding file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 import { type NextPage } from 'next';
+import Loading from '@components/common/loading';
 import { useSession } from 'next-auth/react';
+import HeadComp from '@components/common/headcomponent';
+import Welcome from '@components/home/welcome';
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
 
-  return <></>;
+  if (status === 'unauthenticated') {
+    return (
+      <>
+        <HeadComp></HeadComp>
+      </>
+    );
+  } else if (status === 'authenticated') {
+    return (
+      <>
+        <Welcome></Welcome>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
 };
 
 export default Home;
