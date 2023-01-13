3dbaedbdb8ad186b3e71245deef97d52457420f6
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 00:36:12 +0530

    adding basic file

---
+++ src/components/common/home/welcome.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+import HeadComp from '../headcomponent';
+
+const Welcome: FC = () => {
+  return (
+    <>
+      <HeadComp></HeadComp>
+    </>
+  );
+};
+export default Welcome;
