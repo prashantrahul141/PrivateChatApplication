55b5e6bc5c816e62e9f0581977e71292189d48df
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 00:35:38 +0530

    adding theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        themePrimary: {
+          100: '#fff5f5',
+          200: '#fcb7b8',
+          300: '#f67a7c',
+          400: '#ee4548',
+          500: '#e21c1f',
+          600: '#d20004',
+          700: '#bd0003',
+          800: '#a30003',
+          900: '#830002',
+          1000: '#5f0002',
+          1100: '#3b0001',
+        },
+      },
+    },
   },
   plugins: [],
 };
