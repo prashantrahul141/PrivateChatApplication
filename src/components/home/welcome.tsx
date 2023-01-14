f221cbb342fbf00da5c348bb00728b858e9df184
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 20:17:06 +0530

    basic welcome page

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -1,10 +1,42 @@
 import type { FC } from 'react';
 import HeadComp from '../common/headcomponent';
+import Link from 'next/link';
 
 const Welcome: FC = () => {
   return (
     <>
       <HeadComp></HeadComp>
+      <div className='flex h-screen w-screen items-center justify-center '>
+        <div className='h-max w-full max-w-xl text-center'>
+          {/* title */}
+          <div className=''>
+            <span className='bg-gradient-to-r from-themePrimary-200 to-themePrimary-400 bg-clip-text font-righteous text-5xl tracking-wide text-transparent sm:text-6xl'>
+              Private Chat
+            </span>
+          </div>
+
+          {/* sub text */}
+          <div className='mt-2'>
+            <span className='font-ubuntu text-themePrimary-100/70'>
+              Privacy while chatting? easy.
+            </span>
+          </div>
+
+          {/* buttons */}
+          <div className='mt-24 flex items-center justify-center '>
+            <Link href='/signin'>
+              <button className='btn-signin h-max w-max px-3 font-ubuntu text-sm capitalize'>
+                Sign up for free
+              </button>
+            </Link>
+            <Link href={'/about'}>
+              <button className='btn-signin h-max w-max border border-themePrimary-300 bg-transparent  px-3 font-ubuntu text-sm  capitalize hover:bg-themePrimary-300/10'>
+                Learn More
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
