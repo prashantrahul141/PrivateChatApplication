832aa0ed3caaebad40883bdc212cd3e5bfc99983
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 16:18:26 +0530

    adding c omponent

---
+++ src/components/common/nonlogintopbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+const NonLoginTopBar = () => {
+  return (
+    <>
+      <div className='fixed left-0 top-0 flex h-12 w-screen justify-center border-b border-b-themePrimary-100/20 '>
+        <div className='flex h-full w-full max-w-7xl'>
+          <Link className='my-auto' href={'/'}>
+            <span className='ml-2 font-righteous text-3xl text-themePrimary-300'>
+              PCA
+            </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NonLoginTopBar;
