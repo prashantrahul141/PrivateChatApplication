d7dfedd1f80bcc4b07ce500913efbf637946e4e7
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-14 22:25:17 +0530

    adding some padding

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -24,13 +24,13 @@ const Welcome: FC = () => {
 
           {/* buttons */}
           <div className='mt-24 flex items-center justify-center '>
-            <Link href='/signin'>
-              <button className='btn-signin h-max w-max px-3 font-ubuntu text-sm capitalize'>
+            <Link href='/signin' className='mx-3'>
+              <button className='btn h-max w-max px-3 text-sm capitalize'>
                 Sign up for free
               </button>
             </Link>
-            <Link href={'/about'}>
-              <button className='btn-signin h-max w-max border border-themePrimary-300 bg-transparent  px-3 font-ubuntu text-sm  capitalize hover:bg-themePrimary-300/10'>
+            <Link href={'/about'} className='mx-3'>
+              <button className='btn  h-max w-max border border-themePrimary-300  bg-transparent px-3 text-sm  capitalize hover:bg-themePrimary-300/10'>
                 Learn More
               </button>
             </Link>
