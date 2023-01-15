d03d08bb7dc30128e9018e6e6462e15e314981c8
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-15 07:57:46 +0530

    adding component

---
+++ src/components/common/topbar.tsx
@@ -0,0 +1,68 @@
+import { signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import type { FC } from 'react';
+import { useState } from 'react';
+
+const TopBar: FC<{ image: string | null | undefined }> = ({ image }) => {
+  const [showOptionsMenu, setShowOptionsMenu] = useState(true);
+
+  return (
+    <>
+      <div
+        className='fixed left-0 top-0 flex h-12 w-screen justify-center border-b border-b-themePrimary-100/20 backdrop-blur-lg'
+        onMouseLeave={() => {
+          setShowOptionsMenu(false);
+        }}>
+        <div className='relative flex h-full w-full max-w-7xl'>
+          {/* Icon */}
+          <Link className='my-auto' href={'/'}>
+            <span className='ml-4 font-righteous text-3xl text-themePrimary-300'>
+              PCA
+            </span>
+          </Link>
+
+          {/* User Avatar */}
+          <div
+            className='my-auto ml-auto mr-6 flex w-8 cursor-pointer'
+            onClick={() => {
+              setShowOptionsMenu(!showOptionsMenu);
+            }}>
+            <Image
+              className='rounded-full'
+              alt='Avatar'
+              src={image ? image : '/static/defaultavatarimage.jpg'}
+              width={50}
+              height={50}></Image>
+            <span className='my-auto text-[0.6rem] text-themePrimary-50'>
+              &nbsp;&#9660;
+            </span>
+          </div>
+          {/* Menu options */}
+          {showOptionsMenu && (
+            <>
+              <div className='absolute right-9 top-11 w-max  rounded border border-themePrimary-200/50 bg-baseBackground/60 '>
+                <div className='navbar-menuitems mt-2'>New Chat</div>
+                <Link href={'/settings'}>
+                  <div className='navbar-menuitems'>Settings</div>
+                </Link>
+                <Link href={'/about'}>
+                  <div className='navbar-menuitems'>About</div>
+                </Link>
+                <div
+                  className='navbar-menuitems mb-2'
+                  onClick={() => {
+                    signOut();
+                  }}>
+                  Sign out
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopBar;
