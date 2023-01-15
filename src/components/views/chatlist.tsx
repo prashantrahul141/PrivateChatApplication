382eb154331fee034a77f48599f788dd66c23c37
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-16 02:16:22 +0530

    rewriting list component according to new schema

--- src/components/views/chatlist.tsx
+++ src/components/views/chatlist.tsx
@@ -2,7 +2,7 @@ import ErrorAlert from '@components/common/erroralert';
 import Loading from '@components/common/loading';
 import TopBar from '@components/common/topbar';
 import ChatListSearch from '@components/forms/chatlistsearch';
-import type { Chat } from '@prisma/client';
+import type { Chat, ChatsOnUsers, User } from '@prisma/client';
 import { api } from '@utils/api';
 import { useSession } from 'next-auth/react';
 import type { FC } from 'react';
@@ -15,7 +15,13 @@ const ChatList: FC = () => {
 
   const { data: session } = useSession();
   const [showUserNotFoundError, setShowUserNotFoundError] = useState(false);
-  const [chatList, setChatlist] = useState<Chat[]>([]);
+  const [chatList, setChatlist] = useState<
+    (Chat & {
+      Users: (ChatsOnUsers & {
+        user: User;
+      })[];
+    })[]
+  >([]);
 
   // initial load
   useEffect(() => {
@@ -27,69 +33,64 @@ const ChatList: FC = () => {
 
   if (querychatlist.isFetched && querychatlist.data !== undefined) {
     // Call back function to create a new chat
-    const createChat = ({ userId }: { userId: string }) => {
+    const createChat = async ({ userId }: { userId: string }) => {
       if (userId === session?.user?.id) {
         setShowUserNotFoundError(true);
         return;
       }
-      createChatMutation.mutate({ userId: userId });
+      const createdChat = await createChatMutation.mutateAsync({
+        userId: userId,
+      });
 
-      if (createChatMutation.isSuccess) {
-        if (createChatMutation.data.status === 404) {
-          setShowUserNotFoundError(true);
-        } else if (
-          createChatMutation.data.status === 201 &&
-          createChatMutation.data.createdChat !== undefined
-        ) {
-          setShowUserNotFoundError(false);
-          setChatlist([...chatList, createChatMutation.data.createdChat]);
-        }
+      if (createdChat.status === 404) {
+        setShowUserNotFoundError(true);
+      } else if (
+        createdChat.status === 201 &&
+        createdChat.createdChat !== undefined
+      ) {
+        setShowUserNotFoundError(false);
+        setChatlist([...chatList, createdChat.createdChat]);
       }
     };
-    if (chatList.length > 0) {
-      return (
-        <>
-          <TopBar image={session?.user?.image}></TopBar>
-          <div className='flex h-screen w-screen justify-center'>
-            <div className='mt-24 h-max w-full max-w-xl border border-themePrimary-200/0'>
-              <ChatListSearch createCallbackFunc={createChat}></ChatListSearch>
+    return (
+      <>
+        <TopBar image={session?.user?.image}></TopBar>
+        <div className='flex h-screen w-screen justify-center'>
+          <div className='mt-24 h-max w-full max-w-xl border border-themePrimary-200/0'>
+            <ChatListSearch createCallbackFunc={createChat}></ChatListSearch>
 
-              {querychatlist.data.chatlist.map((_each_chat_item) => {
-                return (
-                  <div key={_each_chat_item.id} className='text-white'>
-                    {_each_chat_item.id}
-                  </div>
-                );
-              })}
+            <div className='h-full w-full border border-red-200'>
+              {chatList.length > 0 &&
+                chatList.map((_each_chat_item) => {
+                  return (
+                    <div
+                      key={_each_chat_item.Users[0]?.chatId}
+                      className='border border-themePrimary-200 text-red-50'>
+                      <div>{_each_chat_item.Users[1]?.user.image}</div>
+                    </div>
+                  );
+                })}
             </div>
-          </div>
-          {showUserNotFoundError && (
-            <ErrorAlert
-              stateFunction={(_state: boolean) => {
-                setShowUserNotFoundError(_state);
-              }}
-              errorMsg={`Couldn't find your friend :(`}></ErrorAlert>
-          )}
-        </>
-      );
-    } else if (chatList.length === 0) {
-      return (
-        <>
-          <TopBar image={session?.user?.image}></TopBar>
-          <div className='flex h-screen w-screen justify-center'>
-            <div className='mt-24 h-max w-full max-w-xl border border-themePrimary-200/0'>
-              <ChatListSearch createCallbackFunc={createChat}></ChatListSearch>
+
+            {chatList.length === 0 && (
               <div className='mt-64 text-center sm:mt-52'>
                 <span className='font-ubuntu text-themePrimary-50/90 text-themePrimary-50'>
                   You don&apos;t have any chats :/
                   <br /> Click on + icon to start a new chat.
                 </span>
               </div>
-            </div>
+            )}
           </div>
-        </>
-      );
-    }
+        </div>
+        {showUserNotFoundError && (
+          <ErrorAlert
+            stateFunction={(_state: boolean) => {
+              setShowUserNotFoundError(_state);
+            }}
+            errorMsg={`Couldn't find your friend :(`}></ErrorAlert>
+        )}
+      </>
+    );
   }
 
   return (
