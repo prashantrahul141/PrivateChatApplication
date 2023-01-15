f4e4d4e2ea87bb87f67085257893d3340824e854
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-15 15:37:35 +0530

    getChatList, createChat

--- src/server/api/routers/mainrouter.ts
+++ src/server/api/routers/mainrouter.ts
@@ -1,10 +1,35 @@
 import { z } from 'zod';
-import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { prisma } from 'src/server/db';
 
 export const mainRouter = createTRPCRouter({
-  sendMessage: protectedProcedure
-    .input(z.object({ message: z.string() }))
-    .query(({ input, ctx }) => {
-      return { message: `created message ${input.message}` };
+  getChatList: protectedProcedure.query(async ({ ctx }) => {
+    const _chatlist = await prisma.chat.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    return { chatlist: _chatlist };
+  }),
+
+  createChat: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const foundUser = await prisma.user.findUnique({
+        where: {
+          id: input.userId,
+        },
+      });
+
+      if (foundUser === null) {
+        return {
+          status: 404,
+        };
+      }
+      const createdChat = await prisma.chat.create({
+        data: { userId: ctx.session.user.id, toUser: input.userId },
+      });
+
+      return { createdChat };
     }),
 });
