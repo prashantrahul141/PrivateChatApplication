54e47bf2160f2e62f600ed7496d2be3008425d07
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-16 21:25:11 +0530

    adding component

--- src/pages/chat/[chatid].tsx
+++ src/pages/chat/[chatid].tsx
@@ -1,7 +1,53 @@
-import type { NextPage } from 'next';
+import HeadComp from '@components/common/headcomponent';
+import Loading from '@components/common/loading';
+import TopBar from '@components/common/topbar';
+import ChatList from '@components/views/chatlist';
+import MessageList from '@components/views/messagelist';
+import type { NextPage, NextPageContext } from 'next';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-const Chat: NextPage = () => {
-  return <></>;
+export const getServerSideProps = (context: NextPageContext) => {
+  const { query } = context;
+  const { chatid } = query;
+  return {
+    props: {
+      chatid: chatid || '#',
+    },
+  };
+};
+
+const Chat: NextPage<{ chatid: string }> = ({ chatid }) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === 'authenticated') {
+    return (
+      <>
+        <HeadComp headTitle='sd'></HeadComp>
+        <TopBar image={session.user?.image}></TopBar>
+        <div className='flex h-screen w-screen items-center justify-center'>
+          <div className='flex w-max border border-red-200'>
+            <div className='hidden lg:block'>
+              <ChatList></ChatList>
+            </div>
+            <div>
+              <MessageList chatid={chatid}></MessageList>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else if (status === 'unauthenticated') {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    router.push('/signin');
+  }
+
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
 };
 
 export default Chat;
