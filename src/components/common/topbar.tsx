d999d15385b28f6738fdfb669249469969d55c55
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-16 18:11:59 +0530

    removing navbar options when clicked anywhere on the screen

--- src/components/common/topbar.tsx
+++ src/components/common/topbar.tsx
@@ -10,11 +10,7 @@ const TopBar: FC<{ image: string | null | undefined }> = ({ image }) => {
 
   return (
     <>
-      <div
-        className='fixed left-0 top-0 z-30 flex h-12 w-screen justify-center border-b border-b-themePrimary-100/20 backdrop-blur-lg'
-        onMouseLeave={() => {
-          setShowOptionsMenu(false);
-        }}>
+      <div className='fixed left-0 top-0 z-30 flex h-12 w-screen justify-center border-b border-b-themePrimary-100/20 backdrop-blur-lg'>
         <div className='relative flex h-full w-full max-w-7xl'>
           {/* Icon */}
           <Link className='my-auto' href={'/'}>
@@ -42,30 +38,42 @@ const TopBar: FC<{ image: string | null | undefined }> = ({ image }) => {
           {/* Menu options */}
           <AnimatePresence>
             {showOptionsMenu && (
-              <motion.div
-                key={'navbar-menuoptions'}
-                className='absolute right-9 top-11 w-max  rounded border border-themePrimary-200/50 bg-baseBackground-500/80'
-                initial={{ opacity: 0, scale: 0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0 }}
-                transition={{ type: 'spring', duration: 0.2 }}>
-                <Link href={'/'}>
-                  <div className='navbar-menuitems mt-2'>Home</div>
-                </Link>
-                <Link href={'/settings'}>
-                  <div className='navbar-menuitems'>Settings</div>
-                </Link>
-                <Link href={'/about'}>
-                  <div className='navbar-menuitems'>About</div>
-                </Link>
-                <div
-                  className='navbar-menuitems mb-2'
-                  onClick={() => {
-                    void signOut();
-                  }}>
-                  Sign out
-                </div>
-              </motion.div>
+              <div
+                className='fixed h-screen w-screen'
+                onClick={(e) => {
+                  const divElement = e.target as HTMLDivElement;
+                  console.log(divElement.id);
+
+                  if (divElement.id === 'menuoptions-id') {
+                    setShowOptionsMenu(false);
+                  }
+                }}
+                id='menuoptions-id'>
+                <motion.div
+                  key={'navbar-menuoptions'}
+                  className='absolute right-9 top-11 w-max  rounded border border-themePrimary-200/50 bg-baseBackground-500/80'
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ type: 'spring', duration: 0.2 }}>
+                  <Link href={'/'}>
+                    <div className='navbar-menuitems mt-2'>Home</div>
+                  </Link>
+                  <Link href={'/settings'}>
+                    <div className='navbar-menuitems'>Settings</div>
+                  </Link>
+                  <Link href={'/about'}>
+                    <div className='navbar-menuitems'>About</div>
+                  </Link>
+                  <div
+                    className='navbar-menuitems mb-2'
+                    onClick={() => {
+                      void signOut();
+                    }}>
+                    Sign out
+                  </div>
+                </motion.div>
+              </div>
             )}
           </AnimatePresence>
         </div>
