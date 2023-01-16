70c9fbdd4ed79975a1bf65e6e6a355353bd2d1f8
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-16 18:05:53 +0530

    animte createlist form

--- src/components/forms/chatlistsearch.tsx
+++ src/components/forms/chatlistsearch.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 import { useRef } from 'react';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const ChatListSearch: FC<{
   createCallbackFunc: ({ userId }: { userId: string }) => Promise<void>;
@@ -49,83 +50,100 @@ const ChatListSearch: FC<{
         </div>
       </div>
 
-      {/* create chat menu */}
-      {showCreateChat && (
-        <div
-          onClick={(e) => {
-            const divElement = e.target as HTMLDivElement;
-            if (divElement.id === 'backdrop-id') {
-              setShowCreateChat(false);
-              setButtonDisabled(true);
-            }
-          }}
-          id='backdrop-id'
-          className='fixed top-0 left-0 z-40 flex h-full w-full items-center justify-center backdrop-blur-sm backdrop-brightness-75'>
-          <div className='z-50 mx-1 h-max w-full max-w-lg rounded-md bg-baseBackground-400 px-4 py-7'>
-            <div className='mb-8'>
-              <span className='font-ubuntu text-lg text-themePrimary-50'>
-                Create new chat
-              </span>
-            </div>
-            <div className='mb-6'>
-              <input
-                onChange={() => {
-                  if (inputRef !== null && inputRef.current !== null) {
-                    setCreateInputValue(inputRef.current.value.trim());
-                    setButtonDisabled(
-                      inputRef.current.value.trim().length === 0
-                    );
-                  }
-                }}
-                ref={inputRef}
-                className='input select-none'
-                placeholder={`User ID. Eg: ${EXAMPLE_ID}`}></input>
-            </div>
-            <div className='flex items-center justify-center gap-3'>
-              <div className='mr-auto'>
-                <span className='ml-1 font-ubuntu text-base text-themePrimary-50/70'>
-                  or send&nbsp;
-                  <span
-                    onClick={() => {
-                      if (inputRef !== null && inputRef.current !== null) {
-                        inputRef.current.value = EXAMPLE_ID;
-                        setCreateInputValue(EXAMPLE_ID);
-                        setButtonDisabled(
-                          inputRef.current.value.trim().length === 0
-                        );
-                      }
-                    }}
-                    className='cursor-pointer text-themePrimary-300 underline'>
-                    me!
-                  </span>
+      <AnimatePresence>
+        {/* create chat menu */}
+        {showCreateChat && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.15,
+            }}
+            onClick={(e) => {
+              const divElement = e.target as HTMLDivElement;
+              if (divElement.id === 'backdrop-id') {
+                setShowCreateChat(false);
+                setButtonDisabled(true);
+              }
+            }}
+            id='backdrop-id'
+            className='fixed top-0 left-0 z-40 flex h-full w-full items-center justify-center bg-black/60 backdrop-blur-lg'>
+            <motion.div
+              key={'navbar-menu'}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={{
+                type: 'spring',
+                duration: 0.3,
+              }}
+              className='z-50 mx-1 h-max w-full max-w-lg rounded-md bg-baseBackground-400 px-4 py-7'>
+              <div className='mb-8'>
+                <span className='font-ubuntu text-lg text-themePrimary-50'>
+                  Create new chat
                 </span>
               </div>
-              <div className='ml-auto'>
-                <button
-                  onClick={() => {
-                    setShowCreateChat(false);
-                    setButtonDisabled(true);
+              <div className='mb-6'>
+                <input
+                  onChange={() => {
+                    if (inputRef !== null && inputRef.current !== null) {
+                      setCreateInputValue(inputRef.current.value.trim());
+                      setButtonDisabled(
+                        inputRef.current.value.trim().length === 0
+                      );
+                    }
                   }}
-                  className='btn-signin m-0 w-max bg-transparent px-2 font-ubuntu text-xs capitalize hover:bg-themePrimary-200/10 hover:text-themePrimary-50'>
-                  Cancel
-                </button>
+                  ref={inputRef}
+                  className='input select-none'
+                  placeholder={`User ID. Eg: ${EXAMPLE_ID}`}></input>
               </div>
-              <div>
-                <button
-                  disabled={buttonDisabled}
-                  className='btn px-2 font-ubuntu text-xs capitalize'
-                  onClick={async (e) => {
-                    e.preventDefault();
-                    setShowCreateChat(false);
-                    await createCallbackFunc({ userId: createInputValue });
-                  }}>
-                  Create
-                </button>
+              <div className='flex items-center justify-center gap-3'>
+                <div className='mr-auto'>
+                  <span className='ml-1 font-ubuntu text-base text-themePrimary-50/70'>
+                    or send&nbsp;
+                    <span
+                      onClick={() => {
+                        if (inputRef !== null && inputRef.current !== null) {
+                          inputRef.current.value = EXAMPLE_ID;
+                          setCreateInputValue(EXAMPLE_ID);
+                          setButtonDisabled(
+                            inputRef.current.value.trim().length === 0
+                          );
+                        }
+                      }}
+                      className='cursor-pointer text-themePrimary-300 underline'>
+                      me!
+                    </span>
+                  </span>
+                </div>
+                <div className='ml-auto'>
+                  <button
+                    onClick={() => {
+                      setShowCreateChat(false);
+                      setButtonDisabled(true);
+                    }}
+                    className='btn-signin m-0 w-max bg-transparent px-2 font-ubuntu text-xs capitalize hover:bg-themePrimary-200/10 hover:text-themePrimary-50'>
+                    Cancel
+                  </button>
+                </div>
+                <div>
+                  <button
+                    disabled={buttonDisabled}
+                    className='btn px-2 font-ubuntu text-xs capitalize'
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      setShowCreateChat(false);
+                      await createCallbackFunc({ userId: createInputValue });
+                    }}>
+                    Create
+                  </button>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-      )}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
