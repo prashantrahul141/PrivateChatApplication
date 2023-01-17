f6bb07fdeac335e0164badb96280fb69fef2135f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-17 23:12:42 +0530

    adding incomplete component

--- src/components/views/messagelist.tsx
+++ src/components/views/messagelist.tsx
@@ -1,8 +1,68 @@
+import Loading from '@components/common/loading';
+import MessageForm from '@components/forms/messagesendform';
+import type { Message } from '@prisma/client';
 import { api } from '@utils/api';
 import type { FC } from 'react';
+import { useState } from 'react';
+import EachMessageComponent from './eachmessagecomponent';
 
-const MessageList: FC<{ chatid: string }> = ({ chatid }) => {
-  const chat = api.main.getChat.useQuery({ chatid });
-  return <></>;
+const MessageList: FC<{ userId: string; chatid: string }> = ({
+  userId,
+  chatid,
+}) => {
+  const initalChatQuery = api.main.getInitialChat.useQuery({ chatid });
+  const sendMessageMutation = api.main.sendMessage.useMutation();
+  const [messages, setMessages] = useState<Array<Message>>([]);
+
+  // initial data load.
+  useState(() => {
+    if (
+      initalChatQuery.isSuccess &&
+      initalChatQuery.data.foundChat !== undefined
+    ) {
+      setMessages(initalChatQuery.data.foundChat.messages);
+    }
+    // @ts-ignore
+  }, [initalChatQuery.isFetched]);
+
+  if (
+    initalChatQuery.isFetched &&
+    initalChatQuery.data?.foundChat !== undefined
+  ) {
+    // call back function to create new chat
+
+    const callback = async (text: string) => {
+      const createdMessage = await sendMessageMutation.mutateAsync({
+        chatId: chatid,
+        text: text,
+      });
+      if (
+        createdMessage.status === 201 &&
+        createdMessage.createdMessage !== undefined
+      ) {
+        setMessages([...messages, createdMessage.createdMessage]);
+      }
+    };
+    return (
+      <div className='w-full'>
+        <div>
+          <EachMessageComponent
+            userId={userId}
+            _messages={messages}></EachMessageComponent>
+        </div>
+        <div>
+          <MessageForm callback={callback} UserName='Prashant'></MessageForm>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className='flex h-full w-full items-center justify-center'>
+        <Loading></Loading>
+      </div>
+    </>
+  );
 };
 export default MessageList;
